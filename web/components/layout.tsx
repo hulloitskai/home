47934f18cc574c8cc03903c568d20098073eedea
Kai <hulloitskai@gmail.com>
2021-11-13 03:15:37 -0500

    💅 (web) Update top navigation layout

--- web/components/layout.tsx
+++ web/components/layout.tsx
@@ -5,7 +5,7 @@ import { StackProps } from "@chakra-ui/react";
 import { VStack, HStack, Center, Spacer } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 import { Badge } from "@chakra-ui/react";
-import { LinkBox, LinkOverlay } from "@chakra-ui/react";
+import { LinkBox, LinkOverlay, Link } from "@chakra-ui/react";
 import { Tooltip } from "@chakra-ui/react";
 
 export interface LayoutProps extends Omit<StackProps, "direction"> {
@@ -15,38 +15,45 @@ export interface LayoutProps extends Omit<StackProps, "direction"> {
 export const Layout: FC<LayoutProps> = ({ badge, children, ...otherProps }) => (
   <VStack align="stretch" minH="100vh" pb={14} {...otherProps}>
     <HStack px={6} py={4}>
-      <Tooltip
-        label="You are loved."
-        placement="bottom-end"
-        hasArrow
-        _light={{ bg: "gray.900" }}
-      >
-        <LinkBox pos="relative">
-          <Text
-            fontFamily="AppleColorEmoji, sans-serif"
-            fontSize="xl"
-            filter="blur(0.6rem)"
-          >
-            ❤️
-          </Text>
-          <Center pos="absolute" inset={0}>
-            <NextLink href="/" passHref>
-              <LinkOverlay>
-                <Text fontFamily="AppleColorEmoji, sans-serif" fontSize="2xl">
-                  ❤️
-                </Text>
-              </LinkOverlay>
-            </NextLink>
-          </Center>
-        </LinkBox>
-      </Tooltip>
+      <HStack spacing={3}>
+        <Tooltip
+          label="You are loved."
+          placement="bottom-end"
+          hasArrow
+          _light={{ bg: "gray.900" }}
+        >
+          <LinkBox pos="relative">
+            <Text
+              fontFamily="AppleColorEmoji, sans-serif"
+              fontSize="xl"
+              filter="blur(0.6rem)"
+            >
+              ❤️
+            </Text>
+            <Center pos="absolute" inset={0}>
+              <NextLink href="/" passHref>
+                <LinkOverlay>
+                  <Text fontFamily="AppleColorEmoji, sans-serif" fontSize="2xl">
+                    ❤️
+                  </Text>
+                </LinkOverlay>
+              </NextLink>
+            </Center>
+          </LinkBox>
+        </Tooltip>
+        <Badge fontSize="sm" colorScheme="red">
+          <NextLink href="/" passHref>
+            <Link _hover={{ textDecor: "none" }}>It&apos;s Kai</Link>
+          </NextLink>
+        </Badge>
+      </HStack>
       <Spacer />
       {!!badge && (
         <Badge
           fontSize="sm"
           bg="gray.500"
           color="gray.50"
-          _dark={{ bg: "gray.700", color: "gray.400" }}
+          _dark={{ bg: "gray.800", color: "gray.300" }}
         >
           {badge}
         </Badge>
