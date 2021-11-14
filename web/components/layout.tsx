4e82a9e66f33cbb052ec446715c1edbdaa1b8ff5
Kai <hulloitskai@gmail.com>
2021-11-14 14:53:51 -0500

    🐛 (web) Revert upgrade to @chakra-ui/react

--- web/components/layout.tsx
+++ web/components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 import NextLink from "next/link";
 
 import { StackProps } from "@chakra-ui/react";
@@ -7,76 +7,85 @@ import { Text } from "@chakra-ui/react";
 import { Badge } from "@chakra-ui/react";
 import { LinkBox, LinkOverlay, Link } from "@chakra-ui/react";
 import { Tooltip } from "@chakra-ui/react";
+import { useColorModeValue } from "@chakra-ui/react";
 
 export interface LayoutProps extends Omit<StackProps, "direction"> {
   badge?: string;
   badgeTooltip?: string;
 }
 
-const LAYOUT_TOOLTIP_THEME = {
-  hasArrow: true,
-  _light: { bg: "gray.900" },
-};
-
 export const Layout: FC<LayoutProps> = ({
   badge,
   badgeTooltip,
   children,
   ...otherProps
-}) => (
-  <VStack align="stretch" minH="100vh" pb={14} {...otherProps}>
-    <HStack px={6} py={4}>
-      <HStack spacing={3}>
-        <Tooltip
-          label="You are loved."
-          placement="bottom-end"
-          {...LAYOUT_TOOLTIP_THEME}
-        >
-          <LinkBox pos="relative">
-            <Text
-              fontFamily="AppleColorEmoji, sans-serif"
-              fontSize="xl"
-              filter="blur(0.6rem)"
-            >
-              ❤️
-            </Text>
-            <Center pos="absolute" inset={0}>
-              <NextLink href="/" passHref>
-                <LinkOverlay>
-                  <Text fontFamily="AppleColorEmoji, sans-serif" fontSize="2xl">
-                    ❤️
-                  </Text>
-                </LinkOverlay>
-              </NextLink>
-            </Center>
-          </LinkBox>
-        </Tooltip>
-        <Tooltip label="Hullo!" placement="bottom" {...LAYOUT_TOOLTIP_THEME}>
-          <Badge fontSize="sm" colorScheme="red">
-            <NextLink href="/" passHref>
-              <Link _hover={{ textDecor: "none" }}>It&apos;s Kai</Link>
-            </NextLink>
-          </Badge>
-        </Tooltip>
-      </HStack>
-      <Spacer />
-      {!!badge && (
-        <Tooltip
-          label={badgeTooltip}
-          placement="bottom-start"
-          {...LAYOUT_TOOLTIP_THEME}
-        >
-          <Badge
-            fontSize="sm"
-            bg="gray.500"
-            color="gray.50"
-            _dark={{ bg: "gray.800", color: "gray.300" }}
+}) => {
+  const tooltipBg = useColorModeValue("gray.900", undefined);
+  const tooltipStyles = useMemo(
+    () => ({
+      hasArrow: true,
+      bg: tooltipBg,
+    }),
+    [tooltipBg],
+  );
+  return (
+    <VStack align="stretch" minH="100vh" pb={14} {...otherProps}>
+      <HStack px={6} py={4}>
+        <HStack spacing={3}>
+          <Tooltip
+            label="You are loved."
+            placement="bottom-end"
+            {...tooltipStyles}
           >
-            {badge}
-          </Badge>
-        </Tooltip>
-      )}
-    </HStack>
-    {children}
-  </VStack>
-);
+            <LinkBox pos="relative">
+              <Text
+                fontFamily="AppleColorEmoji, sans-serif"
+                fontSize="xl"
+                filter="blur(0.6rem)"
+              >
+                ❤️
+              </Text>
+              <Center pos="absolute" inset={0}>
+                <NextLink href="/" passHref>
+                  <LinkOverlay>
+                    <Text
+                      fontFamily="AppleColorEmoji, sans-serif"
+                      fontSize="2xl"
+                    >
+                      ❤️
+                    </Text>
+                  </LinkOverlay>
+                </NextLink>
+              </Center>
+            </LinkBox>
+          </Tooltip>
+          <Tooltip label="Hullo!" placement="bottom" {...tooltipStyles}>
+            <Badge fontSize="sm" colorScheme="red">
+              <NextLink href="/" passHref>
+                <Link _hover={{ textDecor: "none" }}>It&apos;s Kai</Link>
+              </NextLink>
+            </Badge>
+          </Tooltip>
+        </HStack>
+        <Spacer />
+        {!!badge && (
+          <Tooltip
+            label={badgeTooltip}
+            placement="bottom-start"
+            {...tooltipStyles}
+          >
+            <Badge
+              fontSize="sm"
+              bg="gray.500"
+              color="gray.50"
+              _dark={{ bg: "gray.800", color: "gray.300" }}
+            >
+              {badge}
+            </Badge>
+          </Tooltip>
+        )}
+      </HStack>
+      {children}
+    </VStack>
+  );
+};
--- web/pages/test.tsx
+++ web/pages/test.tsx
@@ -7,6 +7,8 @@ import { Heading, Text } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
 import { useToast } from "components/toast";
+
+import { Layout } from "components/layout";
 import { ClientOnly } from "components/client-only";
 
 const TestPage: NextPage = () => {
@@ -26,51 +28,53 @@ const TestPage: NextPage = () => {
   }, []);
 
   return (
-    <Container h="100vh" py={12}>
-      <VStack align="stretch" spacing={8}>
-        <VStack>
-          <Heading size="md">Auth0 Integration</Heading>
-          <ClientOnly>
-            {userJSON && (
-              <Box p={4} bg="gray.100" _dark={{ bg: "gray.800" }}>
-                <Text fontFamily="monospace">{userJSON}</Text>
-              </Box>
-            )}
-          </ClientOnly>
-          <HStack>
-            <Link href={loginURL} _hover={{ textDecor: "none" }}>
-              <Button isDisabled={!!user}>Sign In</Button>
-            </Link>
-            <Link href="/api/auth/logout" _hover={{ textDecor: "none" }}>
-              <Button isDisabled={!user}>Sign Out</Button>
-            </Link>
-          </HStack>
+    <Layout badge="Test" badgeTooltip="Is this thing on?">
+      <Container flex={1} alignSelf="center">
+        <VStack align="stretch" spacing={8}>
+          <VStack>
+            <Heading size="md">Auth0 Integration</Heading>
+            <ClientOnly>
+              {userJSON && (
+                <Box p={4} bg="gray.100" _dark={{ bg: "gray.800" }}>
+                  <Text fontFamily="monospace">{userJSON}</Text>
+                </Box>
+              )}
+            </ClientOnly>
+            <HStack>
+              <Link href={loginURL} _hover={{ textDecor: "none" }}>
+                <Button isDisabled={!!user}>Sign In</Button>
+              </Link>
+              <Link href="/api/auth/logout" _hover={{ textDecor: "none" }}>
+                <Button isDisabled={!user}>Sign Out</Button>
+              </Link>
+            </HStack>
+          </VStack>
+          <VStack>
+            <Heading size="md">Sentry Integration</Heading>
+            <HStack>
+              <Button
+                onClick={() => {
+                  console.error("[TestPage] An error was logged!");
+                }}
+              >
+                Log An Error
+              </Button>
+              <Button
+                onClick={() => {
+                  toast({
+                    status: "error",
+                    title: "Oh noes!",
+                    description: "An error occurred",
+                  });
+                }}
+              >
+                Toast An Error
+              </Button>
+            </HStack>
+          </VStack>
         </VStack>
-        <VStack>
-          <Heading size="md">Sentry Integration</Heading>
-          <HStack>
-            <Button
-              onClick={() => {
-                console.error("[TestPage] An error was logged!");
-              }}
-            >
-              Log An Error
-            </Button>
-            <Button
-              onClick={() => {
-                toast({
-                  status: "error",
-                  title: "Oh noes!",
-                  description: "An error occurred",
-                });
-              }}
-            >
-              Toast An Error
-            </Button>
-          </HStack>
-        </VStack>
-      </VStack>
-    </Container>
+      </Container>
+    </Layout>
   );
 };
 
